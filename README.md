59004b7ac335a6b669df471bd03c13f682330f61
Iiris Rekola <Iiris Rekola>
2016-09-16 15:31:35 +0200

    viikko2 palautus

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-First test on new computer
+###Yatzy###
+
+Yatzy on peli, jossa pelaaja heittää viittä noppaa ja kirjaa tulokset pitetaulukkoon. Vuoro alkaa kaikkia noppia heittämällä. Pelaaja voi sitten kahdesti heittää uudelleen valisemansa nopat. Vuoron lopuksi pelaaja valitsee mille pistetaulukon riville hän haluaa tuloksensa merkitä. Jokaisella rivillä on oma sääntönsä pisteenlaskuun. Pelaaja pyrkiikin maksimoimaan pisteensä valitsemalla jäljellä olevista riveistä itselleen parhaan.
+
+Ohjelmointiprojektin tavoitteena on tuottaa graafisen käyttöliittymän kautta pelattava Yatzy-peli. Mikäli aikaa jää, peliin on tarkoitus lisätä sen eri variantteja.
+
+##Dokumentaatio##
+[aiheen kuvaus](dokumentaatio/aiheenKuvausJaRakenne.md)
+[tuntikirjanpito](dokumentaatio/tuntikirjanpito.md)
--- yatzy/src/main/java/yatzy/Noppa.java
+++ yatzy/src/main/java/yatzy/Noppa.java
@@ -1,4 +1,3 @@
-
 package yatzy;
 
 import java.util.ArrayList;
@@ -7,33 +6,32 @@ import java.util.Random;
 /**
  * Noppaa simuloiva luokka
  */
-
 public class Noppa {
+
     private ArrayList<Integer> arvot;
     private int arvo;
-    private Random arpoja =new Random();
-    
-    
+    private Random arpoja = new Random();
+
     public Noppa(ArrayList<Integer> arvojoukko) {
         this.arvot = arvojoukko;
         this.arvo = uusiArvo();
     }
-    
+
     public int getArvo() {
         return this.arvo;
     }
-    
+
     public ArrayList<Integer> getArvot() {
         return this.arvot;
     }
-    
+
     public int heitaNoppaa() {
         this.arvo = uusiArvo();
         return this.arvo;
     }
-    
+
     public int uusiArvo() {
         return arvot.get(arpoja.nextInt(arvot.size()));
     }
-    
+
 }
